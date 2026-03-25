c8b1658e605c377f0a5ebd2dbb7526f31802a1db
KoolKevin <kevinkoltraka1011@gmail.com>
2026-03-25 11:05:20 +0100

    sad

--- labs/lab1/TestPass.cpp
+++ labs/lab1/TestPass.cpp
@@ -21,6 +21,8 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "llvm/IR/Module.h"
+
 #include <string>
 
 using namespace llvm;
@@ -35,13 +37,52 @@ namespace
     // New PM implementation
     struct TestPass : PassInfoMixin<TestPass>
     {
-        // Main entry point, takes IR unit to run the pass on (&F) and the
-        // corresponding pass manager (to be queried if need be)
-        //
-        // ------------------
-        //
-        // KKoltraka: questo metodo del passo viene invocato
-        // ad ogni funzione
+        PreservedAnalyses run(Module &M, ModuleAnalysisManager &)
+        {
+
+            errs() << "---" << M.getName() << "---" << "\n\n";
+
+            for (Function &F : M)
+            {
+                errs() << F.getName() << "\n\t";
+                errs() << "number of arguments: " << F.arg_size();
+                if (F.isVarArg())
+                    errs() << "+*";
+                errs() << "\n\t";
+                errs() << "number of basic blocks: " << F.size() << "\n\t";
+                errs() << "number of instructions: " << F.getInstructionCount() << "\n\t";
+
+                int callCount = 0;
+                for (BasicBlock &BB : F)
+                {
+                    for (Instruction &I : BB)
+                    {
+                        // Verifica se l'istruzione è una chiamata a funzione
+                        if (auto *Call = dyn_cast<CallInst>(&I))
+                        {
+                            // controllo se la chiamata è verso una funzione che è definita
+                            // nel modulo corrente
+                            Function *CalledF = Call->getCalledFunction();
+                            if (!CalledF->isDeclaration())
+                            {
+                                callCount++;
+                            }
+                        }
+                    }
+                }
+                errs() << "number of inter-module calls: " << callCount << "\n";
+            }
+
+            return PreservedAnalyses::all();
+        }
+
+        // // Main entry point, takes IR unit to run the pass on (&F) and the
+        // // corresponding pass manager (to be queried if need be)
+        // //
+        // // ------------------
+        // //
+        // // KKoltraka: questo metodo del passo viene invocato
+        // // ad ogni funzione
         PreservedAnalyses run(Function &F, FunctionAnalysisManager &)
         {
             errs() << F.getName() << "\n\t";
@@ -85,21 +126,46 @@ namespace
 //-----------------------------------------------------------------------------
 // New PM Registration
 //-----------------------------------------------------------------------------
+// Quando carico il plugin (.so) viene invocata questa funzione per far
+// conoscere il passo a LLVM (lo salva in qualche registro suppongo).
+// Viene anche eseguita la lambda che dice al PassBuilder di aggiungere alla
+// pipeline il passo corrispondente al nome specificato.
+//
+// Invocando: opt -passes=test-pass, il PassBuilder comincia ad eseguire le
+// callback registrate una ad una fino a che una non restituisce true (è stato
+// trovato il passo "test-pass"). A questo punto la callback ha registrato il
+// passo nel rispettivo pass manager ed altri passi dentro il flag passes
+// verranno registrati in un pass manager dal pass builder nello stesso modo.
+// In questo modo si costruisce una pipeline.
+// L'esecuzione della pipeline consiste semplicemente nell'eseguire in ordine
+// tutti i passi registrati nei vari pass manager
 llvm::PassPluginLibraryInfo getTestPassPluginInfo()
 {
     return {
         LLVM_PLUGIN_API_VERSION,
         "TestPass",
         LLVM_VERSION_STRING,
-        // KKoltraka: questo mi sembra serva a registrare il passo
-        // quando invochiamo: 'opt -passes=test-pass'
         [](PassBuilder &PB)
         {
+            // per pass su moduli
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, ModulePassManager &MPM,
+                   ArrayRef<PassBuilder::PipelineElement>)
+                {
+                    if (Name == "test-pass-module")
+                    {
+                        MPM.addPass(TestPass());
+                        return true;
+                    }
+                    return false;
+                });
+
+            // // per pass su funzioni
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, FunctionPassManager &FPM,
                    ArrayRef<PassBuilder::PipelineElement>)
                 {
-                    if (Name == "test-pass")
+                    if (Name == "test-pass-function")
                     {
                         FPM.addPass(TestPass());
                         return true;
